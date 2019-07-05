7d59c57909f23413e09740de7a1d765b0f270e09
Nguyen Tich Duy <nguyentichduy10a15cmg@gmailc.com>
2019-07-05 10:36:19 +0700

    +Add map to resource

---
+++ KingComeBack/Resources/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset" tilewidth="16" tileheight="16" tilecount="8192" columns="64">
+ <image source="terrain-v7.png" width="1024" height="2048"/>
+</tileset>

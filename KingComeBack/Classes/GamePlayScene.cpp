ab86122f72792b2c64e7abf0564396ce7e58381e
Nguyen Tich Duy <nguyentichduy10a15cmg@gmailc.com>
2019-07-08 21:51:27 +0700

    +Add physics of camera and map to GamePlayScene

--- KingComeBack/Classes/GamePlayScene.cpp
+++ KingComeBack/Classes/GamePlayScene.cpp
@@ -9,8 +9,13 @@ GamePlayScene::~GamePlayScene()
 
 Scene * GamePlayScene::createScene()
 {
+	auto scene = Scene::createWithPhysics();
 
-	return GamePlayScene::create();
+	scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+
+	auto layer = GamePlayScene::create();
+	scene->addChild(layer, 0);
+	return scene;
 }
 
 bool GamePlayScene::init()
@@ -19,38 +24,22 @@ bool GamePlayScene::init()
 	{
 		return false;
 	}
-	auto screenSize = Director::getInstance()->getVisibleSize();
+	screenSize = Director::getInstance()->getVisibleSize();
+	sizeWall = Vec2(3.0f, 3.0f);
 	//add map
-	map = TMXTiledMap::create("map.tmx");
+	this->AddMap();
 
-	map->setAnchorPoint(Vec2(0, 0));
-	map->setPosition(0, 0);
-
-	this->addChild(map, 0);
-
-	// add physic for camera
 	// add camera
-	this->setCameraMask((unsigned short)CameraFlag::USER1, true);
+	this->AddCamera();
 
-	camera = Camera::create();
-	camera->setPosition(map->getContentSize().width / 2, map->getContentSize().height / 2);
-
-	camera->setCameraFlag(CameraFlag::USER1);
-	this->addChild(camera, 1);
-	// move camera
-	auto listener = EventListenerTouchOneByOne::create();
-
-	listener->onTouchBegan = CC_CALLBACK_2(GamePlayScene::OnTouchBegan, this);
-	listener->onTouchMoved = CC_CALLBACK_2(GamePlayScene::OnTouchMove, this);
-
-	this->_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+	//Add move camera
+	this->MoveCamera();
 
 	return true;
 }
 
 bool GamePlayScene::OnTouchBegan(Touch * touch, Event * unused_event)
 {
-
 	return true;
 }
 
@@ -59,4 +48,89 @@ void GamePlayScene::OnTouchMove(Touch * touch, Event * unused_event)
 	touchLocation = touch->getLocation();
 
 	camera->setPosition(touchLocation);
+}
+
+void GamePlayScene::AddMap()
+{
+	map = TMXTiledMap::create("map.tmx");
+
+	map->setAnchorPoint(Vec2(0, 0));
+	map->setPosition(0, 0);
+	this->addChild(map);
+
+	// add physic for map
+
+	auto sizeMap = map->getContentSize();
+
+	auto physicBody = PhysicsBody::createEdgeBox(sizeMap,
+		PHYSICSBODY_MATERIAL_DEFAULT, 3);
+	physicBody->setCollisionBitmask(100);
+	physicBody->setContactTestBitmask(true);
+	physicBody->getShape(0)->setRestitution(5);
+	physicBody->getShape(0)->setDensity(50.0f);
+
+	auto node = Node::create();
+	node->setPosition(Vec2(sizeMap.width /2, sizeMap.height / 2));
+	node->setPhysicsBody(physicBody);
+	this->addChild(node);
+}
+
+void GamePlayScene::AddCamera()
+{
+	this->setCameraMask((unsigned short)CameraFlag::USER1, true);
+
+	camera = Camera::create();
+	camera->setPosition(map->getContentSize().width / 2, map->getContentSize().height / 2);
+
+	camera->setCameraFlag(CameraFlag::USER1);
+
+	auto physicBody = PhysicsBody::createBox(screenSize, 
+		PHYSICSBODY_MATERIAL_DEFAULT);
+	physicBody->setCollisionBitmask(101);
+	physicBody->setContactTestBitmask(true);
+	physicBody->setGravityEnable(false);
+	physicBody->setRotationEnable(false);
+	physicBody->setMass(50);
+
+	camera->setPhysicsBody(physicBody);
+
+	this->addChild(camera);
+}
+
+void GamePlayScene::AddPhysicWall(float _positionX, float _positionY,
+	const float &_width, const float &_height)
+{
+	Size sizeBox = Size(ccp(_width, _height));
+
+	auto spWall = Sprite::create();
+	spWall->setPosition(Vec2(_positionX, _positionY));
+	auto bodyMap = PhysicsBody::createBox(sizeBox, PHYSICSBODY_MATERIAL_DEFAULT);
+	bodyMap->getShape(0)->setRestitution(1.0f);
+	bodyMap->getShape(0)->setFriction(0.0f);
+	bodyMap->getShape(0)->setDensity(1.0f);
+	bodyMap->setGravityEnable(false);
+	bodyMap->setDynamic(false);
+
+	spWall->setPhysicsBody(bodyMap);
+	spWall->addChild(spWall);
+}
+
+void GamePlayScene::AddPhysicCamera()
+{
+	auto bodyCamera = PhysicsBody::createEdgeBox(screenSize, PHYSICSBODY_MATERIAL_DEFAULT, 3);
+
+	camera->addComponent(bodyCamera);
+
+	this->addChild(camera, 3);
+
+}
+
+void GamePlayScene::MoveCamera()
+{
+	auto listener = EventListenerTouchOneByOne::create();
+
+	listener->onTouchBegan = CC_CALLBACK_2(GamePlayScene::OnTouchBegan, this);
+	listener->onTouchMoved = CC_CALLBACK_2(GamePlayScene::OnTouchMove, this);
+
+	this->_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
 }
--- KingComeBack/Classes/GamePlayScene.h
+++ KingComeBack/Classes/GamePlayScene.h
@@ -2,6 +2,8 @@
 #include"cocos2d.h"
 
 USING_NS_CC;
+const float wigthWall = 1;
+const float heightWall = 1;
 
 class GamePlayScene :public cocos2d::Scene
 {
@@ -17,9 +19,17 @@ public:
 	bool OnTouchBegan(Touch *touch, Event *unused_event);
 	void OnTouchMove(Touch *touch, Event *unused_event);
 
+	void AddMap();
+	void AddCamera();
+	void AddPhysicWall(float _positionX, float _positionY, const float &_width, const float &_height);
+	void AddPhysicCamera();
+	void MoveCamera();
+
 	CREATE_FUNC(GamePlayScene);
 private:
+	Size screenSize;
 	Vec2 touchLocation;
 	TMXTiledMap *map;
+	Size sizeWall;
 };
 

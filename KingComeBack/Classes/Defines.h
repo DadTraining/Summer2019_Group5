e1166f2a30350525dcd106200b4f57b8a974a87a
Tuan <minhtuanapro@gmail.com>
2019-07-09 10:36:28 +0700

    + Add file Defines.h

---
+++ KingComeBack/Classes/Defines.h
@@ -0,0 +1,18 @@
+#ifndef _DEFINES_H_
+#define ID_SPRITE_HERO 0
+#define ID_SPRITE_KNIGHT 1
+
+#define ID_SPLIST_HERO 0
+
+#define ID_TITLEMAP_MAP 0
+
+#define ID_BUTTON_PLAY 0
+#define ID_BUTTON_RESUME 1
+#define ID_BUTTON_HOME 2
+
+#define ID_FONT_MARKERFELT 0
+#define ID_FONT_ARIAL 1
+
+#define ID_SOUND_SONTUNG 0
+
+#endif
